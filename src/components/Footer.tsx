5cea236042450417f33edd0ae8a6f5e0c3c5a032
cjephuneh <calebjephuneh@gmail.com>
2024-12-11 10:35:34 +0300

    Refactor Footer component to use new logo image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Mail, Phone, MapPin, Facebook, Twitter, Linkedin, Instagram } from 'lucide-react';
-import logoImage from '../../assets/logo.png';
+import logoImage from '../../assets/footer (1).jpg';
 
 
 const Footer = () => {
